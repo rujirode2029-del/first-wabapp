ce86b6679b8412aeddac2eccccc63f9a989eeb4e
Agenda Dr-Sumet <rujirode2029@gmail.com>
2026-04-07 11:41:46 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.onclick = () => li.remove();
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
